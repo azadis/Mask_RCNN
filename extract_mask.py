c782cdb7b3e0b42967db6ae917b9ff97859a92c4
sazadi <sazadi@berkeley.edu>
2018-01-25 13:28:28 -0800

    modified extract_masks to be class-wise

--- extract_mask.py
+++ extract_mask.py
@@ -13,82 +13,75 @@ import model as modellib
 import visualize
 
 
-
-
-
-# Root directory of the project
-ROOT_DIR = os.getcwd()
-
-# Directory to save logs and trained model
-MODEL_DIR = os.path.join(ROOT_DIR, "logs")
-
-# Local path to trained weights file
-COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
-# Download COCO trained weights from Releases if needed
-if not os.path.exists(COCO_MODEL_PATH):
-    utils.download_trained_weights(COCO_MODEL_PATH)
-
-# Directory of images to run detection on
-IMAGE_DIR = os.path.join(ROOT_DIR, "images")
-
 class InferenceConfig(coco.CocoConfig):
     # Set batch size to 1 since we'll be running inference on
     # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
     GPU_COUNT = 1
     IMAGES_PER_GPU = 1
 
-config = InferenceConfig()
-config.display()
+class extract_mask():
+	def init(self, root_dir, model_log, model_path):
+		self.ROOT_DIR = root_dir # Root directory of the project
+		self.MODEL_DIR = model_log # Directory to save logs and trained model
+		self.COCO_MODEL_PATH = model_path # Local path to trained weights file
 
-# Create model object in inference mode.
-model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+		if not os.path.exists(self.COCO_MODEL_PATH):
+    		utils.download_trained_weights(self.COCO_MODEL_PATH)
 
-# Load weights trained on MS-COCO
-model.load_weights(COCO_MODEL_PATH, by_name=True)
+    	self.config = InferenceConfig()
+		self.config.display()
 
-# COCO Class names
-# Index of the class in the list is its ID. For example, to get ID of
-# the teddy bear class, use: class_names.index('teddy bear')
-class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
-               'bus', 'train', 'truck', 'boat', 'traffic light',
-               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
-               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
-               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
-               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-               'kite', 'baseball bat', 'baseball glove', 'skateboard',
-               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
-               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
-               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
-               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
-               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
-               'teddy bear', 'hair drier', 'toothbrush']
+		# COCO Class names
+		# Index of the class in the list is its ID. For example, to get ID of
+		# the teddy bear class, use: class_names.index('teddy bear')
+		self.class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+		               'bus', 'train', 'truck', 'boat', 'traffic light',
+		               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+		               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+		               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+		               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+		               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+		               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+		               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+		               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+		               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+		               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+		               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+		               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+		               'teddy bear', 'hair drier', 'toothbrush']
 
 
-# Load a random image from the images folder
-# file_names = next(os.walk(IMAGE_DIR))[2]
-# image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
-path = '/home/sazadi/projects/objCompose/exp/person_umbrella'
-image_path = '%s/images/00021840-COCO_train2014_000000522464-fake_B.png'%path
-name = image_path.split('/')[-1]
-image = skimage.io.imread(image_path)
 
-output_path = '%s/masks'%path
-if not os.path.exists(output_path):
-	os.makedirs(output_path)
 
-# Run detection
-results = model.detect([image], verbose=1)
+   	def inference(self, image_path):
+		# Create model object in inference mode.
+		model = modellib.MaskRCNN(mode="inference", model_dir=self.MODEL_DIR, config=self.config)
 
-# Visualize results
-r = results[0]
+		# Load weights trained on MS-COCO
+		model.load_weights(self.COCO_MODEL_PATH, by_name=True)
 
-print([class_names[r['class_ids'][i]] for i in range(len(r['class_ids']))])
+		# path = '/home/sazadi/projects/objCompose/exp/person_umbrella'
+		# image_path = '%s/images/00021840-COCO_train2014_000000522464-fake_B.png'%path
+		name = image_path.split('/')[-1]
+		image = skimage.io.imread(image_path)
 
-print(r['scores'])
-visualize.display_instances(output_path, name, image, r['rois'], r['masks'], r['class_ids'], 
-                            class_names, r['scores'])
+		output_path = '%s/masks'%path
+		if not os.path.exists(output_path):
+			os.makedirs(output_path)
+
+		# Run detection
+		results = model.detect([image], verbose=1)
+
+		# Visualize results
+		r = results[0]
+
+		pred_classes = ([class_names[r['class_ids'][i]] for i in range(len(r['class_ids']))])
+		pred_scores = r['scores']
+
+		visualize.display_instances(output_path, name, image, r['rois'], r['masks'], r['class_ids'], 
+		                            class_names, r['scores'])
+
+		return pred_classes, pred_scores
 
 
 
